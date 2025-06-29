ec1f6064de2a35ce4c567984f071b9a3a5925cab
DenzDy <dydenzellrobyn0527@gmail.com>
2025-06-29 17:30:20 +0800

    feat: mobile hero page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
 	import '../app.css';
     import { Menu } from '@lucide/svelte';
-	let { children } = $props();
+    const { children } = $props();
 </script>
-<button>
-    <Menu/>
-</button>
-{@render children()}
+<nav class="sticky top-0 bg-primary w-screen h-fit p-3 box-border">
+    <button class="align-bottom">
+        <Menu size={32} strokeWidth={3} class=""/>
+    </button>
+</nav>
+<main class="bg-primary h-[calc(100vh-56px)]">
+    {@render children()}
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { ChevronDown } from '@lucide/svelte';
+</script>
+
+<div class="bg-[url($lib/static/images/hero.png)] bg-size-[auto_100%] bg-[rgba(255,255,255,0.4)] bg-blend-lighten bg-center w-full h-full flex flex-col items-center justify-between pt-10">
+    <img id='hero-image' src="src/lib/static/images/mobile-logo.png" alt="The Third Space Logo" class="w-[500px] h-[300px] object-cover">
+    <p id="hero-text" class="text-primary w-full text-wrap p-5 text-center ">
+        a place where people spend their time between their first place (home) and second place (work or school)
+    </p>
+    <ChevronDown/>
+    <div id="stripes" class="w-full">
+        <div class="bg-blue w-full h-[20px]"></div>
+        <div class="bg-orange w-full h-[20px]"></div>
+        <div class="bg-green w-full h-[20px]"></div>
+
+    </div>
+</div>
