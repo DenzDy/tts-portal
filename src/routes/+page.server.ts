637ab05dde85db75525184cfa85250bf1ab8d622
Airiseru <dmdee2004@gmail.com>
2025-06-29 10:59:23 +0800

    chore: use env file for gsheets tab names

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,13 @@
+import { env } from '$env/dynamic/private';
+import { FORM_FIELDS_TAB, RESPONSES_TAB } from '$env/static/private';
+
 export async function load({ fetch }) {
-    const res = await fetch("/api/gsheet?tab=Form Fields");
+    const fieldsTab = env.FORM_FIELDS_TAB || FORM_FIELDS_TAB
+    const res = await fetch(`/api/gsheet?tab=${fieldsTab}`);
     const values: string[][] = await res.json();
     const filteredValues = values?.filter(row => row?.[0] !== "");
-    return { values: filteredValues };
+    return {
+        values: filteredValues,
+        responsesTab: env.RESPONSES_TAB || RESPONSES_TAB
+    };
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,21 @@
 <script>
     export let data;
-    const values = data?.values ?? [];
+    const { values = [], responsesTab = "" } = data;
     async function writeToSheet() {
-    console.log("writing to sheet");
-    const res = await fetch("/api/gsheet", {
-        method: 'POST',
-        headers: {
-        'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-        values: [[true, 0, new Date(), 'Test Submit', '2012-34567']],
-        targetTab: 'Form Responses'
-        })
-    });
+        console.log("writing to sheet");
+        const res = await fetch("/api/gsheet", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                values: [[true, 0, new Date(), 'Test Submit', '2012-34567']],
+                targetTab: responsesTab
+            })
+        });
 
-    const result = await res.json();
-    console.log(result);
+        const result = await res.json();
+        console.log(result);
     }
 </script>
 
