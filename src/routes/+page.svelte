2ffd6d6ffc94511b9c7748c4df78ebf8114d3685
Airiseru <dmdee2004@gmail.com>
2025-06-29 10:44:56 +0800

    chore: fix indentation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,29 @@
 <script>
-  export let data;
-  const values = data?.values ?? [];
-  async function writeToSheet() {
+    export let data;
+    const values = data?.values ?? [];
+    async function writeToSheet() {
     console.log("writing to sheet");
     const res = await fetch("/api/gsheet", {
-      method: 'POST',
-      headers: {
+        method: 'POST',
+        headers: {
         'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
+        },
+        body: JSON.stringify({
         values: [[true, 0, new Date(), 'Test Submit', '2012-34567']],
         targetTab: 'Form Responses'
-      })
+        })
     });
 
     const result = await res.json();
     console.log(result);
-  }
+    }
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
 {#each values as row}
-  <p>{row}</p>
+    <p>{row}</p>
 {/each}
 
 <button type="button" on:click={writeToSheet} style="background-color: black; color: white">Write to Sheet</button>
