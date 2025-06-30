1978c01f9a9d39af5718114030fd0fb30e359956
DenzDy <dydenzellrobyn0527@gmail.com>
2025-06-30 15:02:30 +0800

    feat: most of mobile landing page; add garet font

---
+++ src/lib/components/zone-card/ZoneCard.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let zoneName, zoneColor, zoneDescription;
+</script>
+
+<div class="flex flex-row gap-5 items-center">
+    <div id="zone-box" class="w-18 h-18 bg-{zoneColor} rounded-md"></div>
+    <div id="zone-text">
+        <h2 class="font-[League_Spartan] font-semibold text-xl">{zoneName} zone</h2>
+        <p class="font-[Comfortaa] text-xs">{zoneDescription}</p>
+    </div>
+</div>
