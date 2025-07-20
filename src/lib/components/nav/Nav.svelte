411cc47087f092b6a2ab9f67dadb8fe3f3844f46
Airiseru <dmdee2004@gmail.com>
2025-07-20 17:30:51 +0800

    fix: navbar reserve

--- src/lib/components/nav/Nav.svelte
+++ src/lib/components/nav/Nav.svelte
@@ -1,50 +1,62 @@
 <script lang="ts">
-    let open = $state(false);
-    import { X, Menu } from '@lucide/svelte';
-    import { slide } from 'svelte/transition';
-    import NavItem from '$lib/components/nav/NavItem.svelte';
-    import desktopLogo from '$lib/static/images/desktop-logo.png';
-    import { onMount } from 'svelte';
+	let open = $state(false);
+	import { X, Menu } from '@lucide/svelte';
+	import { slide } from 'svelte/transition';
+	import NavItem from '$lib/components/nav/NavItem.svelte';
+	import desktopLogo from '$lib/static/images/desktop-logo.png';
+	import { onMount } from 'svelte';
 	let showImage = $state(false);
 	const { container } = $props<{ container: HTMLElement | null }>();
 	onMount(() => {
 		if (!container) return;
 
 		const onScroll = () => {
-			showImage = container.scrollTop > (container.clientHeight / 4);
+			showImage = container.scrollTop > container.clientHeight / 4;
 		};
 
 		container.addEventListener('scroll', onScroll);
 		return () => container.removeEventListener('scroll', onScroll);
 	});
-    const closeMenu = () => (open = false);
+	const closeMenu = () => (open = false);
 </script>
-<nav class="sticky top-0 bg-primary w-screen h-[56px] sm:h-[80px] p-3 box-border sm:items-center sm:justify-between sm:px-5 sm:flex sm:flex-row">
-    <button class="align-bottom sm:hidden" onclick={() => open = !open}>
-        {#if !open}
-            <Menu size={32} strokeWidth={3} class=""/>
-        {:else}
-            <X size={32} strokeWidth={3} class=""/>
-        {/if}
-    </button>
-    {#if open}
-        <div class="left-0 absolute z-[999] sm:hidden mt-2 flex flex-col bg-primary w-full" transition:slide>
-            <NavItem link='/#hero' name='Home' onClick={closeMenu}/>
-            <NavItem link='/#about-us' name='About Us' onClick={closeMenu}/>
-            <NavItem link='/#zones' name='Zones' onClick={closeMenu}/>
-            <NavItem link='/#recent-events' name='Recent Events' onClick={closeMenu}/>
-            <NavItem link='/#contact-us' name='Contact Us' onClick={closeMenu}/>
-            <NavItem link='/reserve' name='Reservations' onClick={closeMenu}/>
-        </div>
-    {/if}
-    <img src={desktopLogo} alt="" srcset="" class="hidden sm:block h-full transition-opacity duration-300 opacity-0" class:opacity-100={showImage}>
-        
-    <div class="hidden sm:block ">
-        <NavItem link='/' name='Home' onClick={closeMenu}/>
-        <NavItem link='/#about-us' name='About Us' onClick={closeMenu}/>
-        <NavItem link='/#zones' name='Zones' onClick={closeMenu}/>
-        <NavItem link='/#recent-events' name='Recent Events' onClick={closeMenu}/>
-        <NavItem link='/#contact-us' name='Contact Us' onClick={closeMenu}/>
-        <NavItem link='/reserve' name='Reservations' onClick={closeMenu}/>
-    </div>
+
+<nav
+	class="bg-primary sticky top-0 box-border h-[56px] w-screen p-3 sm:flex sm:h-[80px] sm:flex-row sm:items-center sm:justify-between sm:px-5"
+>
+	<button class="align-bottom sm:hidden" onclick={() => (open = !open)}>
+		{#if !open}
+			<Menu size={32} strokeWidth={3} class="" />
+		{:else}
+			<X size={32} strokeWidth={3} class="" />
+		{/if}
+	</button>
+	{#if open}
+		<div
+			class="bg-primary absolute left-0 z-[999] mt-2 flex w-full flex-col sm:hidden"
+			transition:slide
+		>
+			<NavItem link="/#hero" name="Home" onClick={closeMenu} />
+			<NavItem link="/#about-us" name="About Us" onClick={closeMenu} />
+			<NavItem link="/#zones" name="Zones" onClick={closeMenu} />
+			<NavItem link="/#recent-events" name="Recent Events" onClick={closeMenu} />
+			<NavItem link="/#contact-us" name="Contact Us" onClick={closeMenu} />
+			<NavItem link="/reserve" name="Reserve" itemClass={['text-orange']} onClick={closeMenu} />
+		</div>
+	{/if}
+	<img
+		src={desktopLogo}
+		alt=""
+		srcset=""
+		class="hidden h-full opacity-0 transition-opacity duration-300 sm:block"
+		class:opacity-100={showImage}
+	/>
+
+	<div class="hidden sm:block">
+		<NavItem link="/" name="Home" onClick={closeMenu} />
+		<NavItem link="/#about-us" name="About Us" onClick={closeMenu} />
+		<NavItem link="/#zones" name="Zones" onClick={closeMenu} />
+		<NavItem link="/#recent-events" name="Recent Events" onClick={closeMenu} />
+		<NavItem link="/#contact-us" name="Contact Us" onClick={closeMenu} />
+		<NavItem link="/reserve" name="Reserve" itemClass={['text-orange']} onClick={closeMenu} />
+	</div>
 </nav>
--- src/lib/components/nav/NavItem.svelte
+++ src/lib/components/nav/NavItem.svelte
@@ -1,6 +1,22 @@
 <script lang="ts">
-    export let name;
-    export let link;
-    export let onClick: () => void;
+	import { cn } from '$lib/utils';
+	import type { ClassValue } from 'clsx';
+	export let name;
+	export let link;
+	export let itemClass: ClassValue[] = [];
+	export let onClick: () => void;
 </script>
-<a href={link} class="ml-4 py-4 sm:px-2 font-[League_Spartan] text-[1.25rem]" onclick={onClick}>{name}</a>
+
+<a
+	href={link}
+	class={cn(
+		'ml-4',
+		'py-4',
+		'sm:px-2',
+		'font-[League_Spartan]',
+		'text-[1.25rem]',
+		'font-semibold',
+		itemClass
+	)}
+	onclick={onClick}>{name}</a
+>
